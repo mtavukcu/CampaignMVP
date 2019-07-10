ebce2c7cc105205b4a8425358b5d857e5d7ceb1b
mtavukcuoglu <merttav@gmail.com>
2019-07-10 16:29:56 +0300

    Build a retrofit client Set API base url to endpoint provided for the app

---
+++ app/src/main/java/com/example/campaignmvp/network/RetrofitClient.kt
@@ -0,0 +1,12 @@
+package com.example.campaignmvp.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitClient {
+    companion object {
+        fun getClient(): Retrofit = Retrofit.Builder()
+            .baseUrl("http://private-d190ab-campaigns19.apiary-mock.com")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+    }
+}
