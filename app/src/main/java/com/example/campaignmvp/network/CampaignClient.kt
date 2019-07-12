773b1312e899837a1096c6eea359123969067fd7
mtavukcuoglu <merttav@gmail.com>
2019-07-12 09:11:19 +0300

    Rename retrofit client class

--- app/src/main/java/com/example/campaignmvp/network/CampaignClient.kt
+++ app/src/main/java/com/example/campaignmvp/network/CampaignClient.kt
@@ -3,7 +3,7 @@ package com.example.campaignmvp.network
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitClient {
+class CampaignClient {
     companion object {
         fun getClient(): Retrofit = Retrofit.Builder()
             .baseUrl("http://private-d190ab-campaigns19.apiary-mock.com")
